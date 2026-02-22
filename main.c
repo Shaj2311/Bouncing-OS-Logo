1751ad18b491a560c186b7b559c3448de9703506
Shaj2311 <haideralijafri2007@gmail.com>
2026-02-22 16:15:42 +0500

    Store art in memory instead

--- main.c
+++ main.c
@@ -3,10 +3,31 @@
 #include <string.h>
 int main()
 {
-	if(system("fastfetch --pipe false > artFile") != 0)
+	char* buf = malloc(256);
+	char** art = malloc(128 * sizeof(char*));
+	for(int i = 0; i < 128; i++)
+	{
+		art[i] = malloc(2048);
+	}
+
+	FILE* artFile = popen("fastfetch --pipe false --structure none", "r");
+	if(!artFile)
 	{
 		puts("An error occured");
 		puts("Is fastfetch installed?");
 		exit(1);
+	}
+
+	int i = 0;
+	while(fgets(buf, 256, artFile))
+	{
+		if(i >= 128) break;
+		strcpy(art[i], buf);
+		i++;
+	}
+
+	for(int j = 0; j < i; j++)
+	{
+		printf("%s", art[j]);
 	}
 }
